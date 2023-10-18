202e67a0ea4c3b6c621236c59affdc75e994936c
PabloPicas0 <pawel.fe@gmail.com>
2023-10-18 23:20:58 +0200

    feat: create basic front concept of main page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,19 @@
-import { useState } from "react";
-
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div>
       <header>
-        <h1>Create or join to the room</h1>
+        <h1 className="text-3xl font-bold text-gray-100">Create or join to the room</h1>
       </header>
 
       <div style={{ display: "flex", gap: "40px", justifyContent: "center" }}>
-        <div style={{ border: "1px solid black", borderRadius: "15px", padding: "20px", width: "163px" }}>
-          <p>Create task room</p>
+        <div className="border border-slate-400 rounded-lg" style={{ padding: "20px", width: "163px" }}>
+          <p className="text-gray-100">Create task room</p>
         </div>
 
-        <div style={{ border: "1px solid black", borderRadius: "15px", padding: "20px", width: "163px" }}>
-          <p>Join task room</p>
+        <div className="border border-slate-400 rounded-lg" style={{ padding: "20px", width: "163px" }}>
+          <p className="text-gray-100">Join task room</p>
         </div>
       </div>
     </div>
