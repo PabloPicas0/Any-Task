0215afdfa4652d6a26ac6c6aacf51c7c5a6f5997
PabloPicas0 <pawel.fe@gmail.com>
2023-10-19 17:52:15 +0200

    feat: complete main page layout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,21 +2,49 @@ import "./App.css";
 
 function App() {
   return (
-    <div>
-      <header>
+    <>
+      <header className="mb-28">
         <h1 className="text-3xl font-bold text-gray-100">Create or join to the room</h1>
       </header>
 
-      <div style={{ display: "flex", gap: "40px", justifyContent: "center" }}>
-        <div className="border border-slate-400 rounded-lg" style={{ padding: "20px", width: "163px" }}>
-          <p className="text-gray-100">Create task room</p>
-        </div>
+      <div className="flex gap-11 items-center flex-col sm:flex-row">
+          <button className="flex flex-col justify-center items-center border border-slate-400 rounded-lg p-5 w-44 h-36 gap-4">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-7 h-7 text-gray-100">
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M11.35 3.836c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m8.9-4.414c.376.023.75.05 1.124.08 1.131.094 1.976 1.057 1.976 2.192V16.5A2.25 2.25 0 0118 18.75h-2.25m-7.5-10.5H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V18.75m-7.5-10.5h6.375c.621 0 1.125.504 1.125 1.125v9.375m-8.25-3l1.5 1.5 3-3.75"
+              />
+            </svg>
 
-        <div className="border border-slate-400 rounded-lg" style={{ padding: "20px", width: "163px" }}>
-          <p className="text-gray-100">Join task room</p>
-        </div>
+            <p className="text-gray-100">Create task room</p>
+          </button>
+
+          <button className="flex flex-col justify-center items-center border border-slate-400 rounded-lg p-5 w-44 h-36 gap-4">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-7 h-7 text-gray-100">
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9"
+              />
+            </svg>
+
+            <p className="text-gray-100">Join task room</p>
+          </button>
       </div>
-    </div>
+    </>
   );
 }
 
