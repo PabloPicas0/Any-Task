a9732f0ac38d899c0966d13ba8d0bc65bf04eace
PabloPicas0 <pawel.fe@gmail.com>
2023-10-22 15:48:30 +0200

    feat: add Floating Action Button

--- client/src/Routes/Room.tsx
+++ client/src/Routes/Room.tsx
@@ -1,5 +1,8 @@
-import { Box, Typography } from "@mui/material";
+import { Add, Edit } from "@mui/icons-material";
+import { Box, Fab, Tooltip, Typography } from "@mui/material";
 import { useParams } from "react-router-dom";
+
+const roomStyles = {};
 
 const Room = () => {
   const { id } = useParams();
@@ -7,6 +10,18 @@ const Room = () => {
   return (
     <Box>
       <Typography color={"white"}>{id}</Typography>
+
+      <Tooltip title="Add task" placement="left">
+        <Fab aria-label="Add todo" sx={{ position: "fixed", right: 20, bottom: 96 }}>
+          <Add />
+        </Fab>
+      </Tooltip>
+
+      <Tooltip title="Edit task" placement="left">
+        <Fab aria-label="Add todo" sx={{ position: "fixed", right: 20, bottom: 20 }}>
+          <Edit />
+        </Fab>
+      </Tooltip>
     </Box>
   );
 };
