5b34c267f49279431f4ac9fc195ede510f95533f
PabloPicas0 <pawel.fe@gmail.com>
2023-10-22 16:48:34 +0200

    feat: create basic todo front

--- client/src/Routes/Room.tsx
+++ client/src/Routes/Room.tsx
@@ -1,6 +1,7 @@
-import { Add, Edit } from "@mui/icons-material";
-import { Box, Fab, Tooltip, Typography } from "@mui/material";
+import { Add, CheckBox, CheckBoxOutlineBlank, Edit, Settings } from "@mui/icons-material";
+import { Box, Button, Fab, Tooltip, Typography } from "@mui/material";
 import { useParams } from "react-router-dom";
+import Task from "../UI/Task";
 
 const roomStyles = {};
 
@@ -8,21 +9,61 @@ const Room = () => {
   const { id } = useParams();
 
   return (
-    <Box>
-      <Typography color={"white"}>{id}</Typography>
+    <>
+      <Box id="todos" sx={{ display: "flex", border: "1px dashed white", height: "354px" }}>
+        <Box
+          sx={{
+            borderRight: "1px dashed white",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            paddingX: "20px",
+            gap: "20px",
+          }}>
+          <Button sx={{ justifyContent: "start" }} startIcon={<CheckBoxOutlineBlank />}>
+            Active
+          </Button>
+
+          <Button sx={{ justifyContent: "start" }} startIcon={<CheckBox />}>
+            Completed
+          </Button>
+        </Box>
+
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "start",
+            paddingY: "20px",
+            overflowY: "auto",
+            width: "100%",
+          }}>
+          <Task />
+          <Task />
+          <Task />
+          <Task />
+          <Task />
+        </Box>
+      </Box>
 
       <Tooltip title="Add task" placement="left">
-        <Fab aria-label="Add todo" sx={{ position: "fixed", right: 20, bottom: 96 }}>
+        <Fab aria-label="Add todo" sx={{ position: "fixed", right: 20, bottom: 172 }}>
           <Add />
         </Fab>
       </Tooltip>
 
       <Tooltip title="Edit task" placement="left">
-        <Fab aria-label="Add todo" sx={{ position: "fixed", right: 20, bottom: 20 }}>
+        <Fab aria-label="Add todo" sx={{ position: "fixed", right: 20, bottom: 96 }}>
           <Edit />
         </Fab>
       </Tooltip>
-    </Box>
+
+      <Tooltip title="Room permissions" placement="left">
+        <Fab aria-label="Add todo" sx={{ position: "fixed", right: 20, bottom: 20 }}>
+          <Settings />
+        </Fab>
+      </Tooltip>
+    </>
   );
 };
 
