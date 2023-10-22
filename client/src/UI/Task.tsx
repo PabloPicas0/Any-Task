367056d58d43f2ae6756972748889a2878e1ea32
PabloPicas0 <pawel.fe@gmail.com>
2023-10-22 16:48:53 +0200

    feat: create basic task component

---
+++ client/src/UI/Task.tsx
@@ -0,0 +1,11 @@
+import { Box, Typography } from "@mui/material";
+
+const Task = () => {
+  return (
+    <Box sx={{padding: "1.2rem 1rem" }}>
+      <Typography>TODO this</Typography>
+    </Box>
+  );
+};
+
+export default Task;
