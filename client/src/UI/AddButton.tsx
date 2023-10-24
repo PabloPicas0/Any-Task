50b47437aeb84ca19ed2291de4acdff359f5e836
PabloPicas0 <pawel.fe@gmail.com>
2023-10-24 19:40:08 +0200

    feat: add simple dialog

--- client/src/UI/AddButton.tsx
+++ client/src/UI/AddButton.tsx
@@ -1,13 +1,34 @@
 import { Add } from "@mui/icons-material";
-import { Tooltip, Fab } from "@mui/material";
+import { Tooltip, Fab, Dialog, DialogTitle, DialogContent, TextField, DialogActions, Button } from "@mui/material";
+
+import { useState } from "react";
 
 const AddButton = () => {
+  const [open, setOpen] = useState<boolean>(false);
+
   return (
-    <Tooltip title="Add task" placement="left">
-      <Fab aria-label="Add todo" sx={{ position: "fixed", right: 20, bottom: 172 }}>
-        <Add />
-      </Fab>
-    </Tooltip>
+    <>
+      <Tooltip title="Add task" placement="left">
+        <Fab
+          aria-label="Add todo"
+          sx={{ position: "fixed", right: 20, bottom: 172 }}
+          onClick={() => setOpen(true)}>
+          <Add />
+        </Fab>
+      </Tooltip>
+
+      <Dialog onClose={() => setOpen(false)} open={open}>
+        <DialogTitle>Add task</DialogTitle>
+
+        <DialogContent>
+          <TextField label="TODO" margin="dense" />
+        </DialogContent>
+
+        <DialogActions>
+          <Button variant="contained">Add to list</Button>
+        </DialogActions>
+      </Dialog>
+    </>
   );
 };
 
