c012ea8d3dca717f37b2d9dea0775d7e0960e815
PabloPicas0 <pawel.fe@gmail.com>
2023-10-25 23:24:28 +0200

    feat: create dialog for permissions options

--- client/src/UI/SettingsButton.tsx
+++ client/src/UI/SettingsButton.tsx
@@ -1,13 +1,71 @@
 import { Settings } from "@mui/icons-material";
-import { Tooltip, Fab } from "@mui/material";
+import {
+  Tooltip,
+  Fab,
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  Box,
+  Switch,
+  Typography,
+} from "@mui/material";
+
+import { useState } from "react";
 
 const SettingsButton = () => {
+  const [open, setOpen] = useState<boolean>(false);
+
   return (
-    <Tooltip title="Room permissions" placement="left">
-      <Fab aria-label="Add todo" sx={{ position: "fixed", right: 20, bottom: 20 }}>
-        <Settings />
-      </Fab>
-    </Tooltip>
+    <>
+      <Tooltip title="Room permissions" placement="left">
+        <Fab
+          aria-label="Add todo"
+          sx={{ position: "fixed", right: 20, bottom: 20 }}
+          onClick={() => setOpen(true)}>
+          <Settings />
+        </Fab>
+      </Tooltip>
+
+      <Dialog
+        onClose={() => setOpen(false)}
+        open={open}
+        PaperProps={{ sx: { width: "100%", maxWidth: "300px" } }}>
+        <DialogTitle textAlign={"center"}>Permissions</DialogTitle>
+
+        <DialogContent>
+          <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+            <Switch />
+            <Typography>test 1</Typography>
+          </Box>
+
+          <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+            <Switch />
+            <Typography>test 2</Typography>
+          </Box>
+
+          <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+            <Switch />
+            <Typography>test 3</Typography>
+          </Box>
+
+          <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+            <Switch />
+            <Typography>test 4</Typography>
+          </Box>
+
+          <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+            <Switch />
+            <Typography>test 5</Typography>
+          </Box>
+        </DialogContent>
+
+        <DialogActions sx={{ justifyContent: "center" }}>
+          <Button variant="contained">Edit permissions</Button>
+        </DialogActions>
+      </Dialog>
+    </>
   );
 };
 
