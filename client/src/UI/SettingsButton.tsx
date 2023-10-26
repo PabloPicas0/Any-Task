1d8d44d9742e97718010cb0d21615ae7064540e6
PabloPicas0 <pawel.fe@gmail.com>
2023-10-26 22:26:53 +0200

    refactor: abstraction of styles

--- client/src/UI/SettingsButton.tsx
+++ client/src/UI/SettingsButton.tsx
@@ -14,54 +14,69 @@ import {
 
 import { useState } from "react";
 
+const settingsButtonStyles = {
+  settingsBtn: {
+    position: "fixed",
+    right: 20,
+    bottom: 20,
+  },
+  dialogPaperProps: {
+    sx: {
+      width: "100%",
+      maxWidth: "300px",
+    },
+  },
+  options: {
+    display: "flex",
+    justifyContent: "space-between",
+  },
+  justifyContent: {
+    justifyContent: "center",
+  },
+};
+
 const SettingsButton = () => {
   const [open, setOpen] = useState<boolean>(false);
 
   return (
     <>
       <Tooltip title="Room permissions" placement="left">
-        <Fab
-          aria-label="Add todo"
-          sx={{ position: "fixed", right: 20, bottom: 20 }}
-          onClick={() => setOpen(true)}>
+        <Fab aria-label="Add todo" sx={settingsButtonStyles.settingsBtn} onClick={() => setOpen(true)}>
           <Settings />
         </Fab>
       </Tooltip>
 
-      <Dialog
-        onClose={() => setOpen(false)}
-        open={open}
-        PaperProps={{ sx: { width: "100%", maxWidth: "300px" } }}>
+      <Dialog onClose={() => setOpen(false)} open={open} PaperProps={settingsButtonStyles.dialogPaperProps}>
         <DialogTitle textAlign={"center"}>Permissions</DialogTitle>
 
         <DialogContent>
-          <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+          <Box sx={settingsButtonStyles.options}>
             <Switch />
             <Typography>test 1</Typography>
           </Box>
 
-          <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+          <Box sx={settingsButtonStyles.options}>
             <Switch />
             <Typography>test 2</Typography>
           </Box>
 
-          <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+          <Box sx={settingsButtonStyles.options}>
             <Switch />
             <Typography>test 3</Typography>
           </Box>
 
-          <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+          <Box sx={settingsButtonStyles.options}>
             <Switch />
             <Typography>test 4</Typography>
           </Box>
 
-          <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+          <Box sx={settingsButtonStyles.options}>
             <Switch />
             <Typography>test 5</Typography>
           </Box>
         </DialogContent>
 
-        <DialogActions sx={{ justifyContent: "center" }}>
+        <DialogActions sx={settingsButtonStyles.justifyContent}>
           <Button variant="contained">Edit permissions</Button>
         </DialogActions>
       </Dialog>
