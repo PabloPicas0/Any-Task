1155fab73745a270ef2d0ad4f6fd837e8525b31b
PabloPicas0 <pawel.fe@gmail.com>
2023-10-30 22:15:46 +0100

    feat: add delete button that is dependent on isEditable state

--- client/src/UI/Task.tsx
+++ client/src/UI/Task.tsx
@@ -1,18 +1,24 @@
-import { Box, Typography } from "@mui/material";
+import { Delete } from "@mui/icons-material";
+import { Box, IconButton, Typography } from "@mui/material";
 
 type TaskProps = {
   description?: string;
   key: number;
+  isEditable: boolean;
 };
 
 const Task = (props: TaskProps) => {
-  const { description } = props;
+  const { description, isEditable } = props;
 
   return (
     <Box sx={{ padding: "1.2rem 3rem" }}>
       <Typography textAlign={"start"} color={"HighlightText"}>
         {description}
       </Typography>
+
+      <IconButton sx={{ display: isEditable ? "inline-flex" : "none" }} disabled={!isEditable}>
+        <Delete />
+      </IconButton>
     </Box>
   );
 };
