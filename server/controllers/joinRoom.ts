ded879d114c8f022822a15df562f7bcfa7e6266d
PabloPicas0 <pawel.fe@gmail.com>
2023-11-04 16:51:12 +0100

    feat: add new users to db

--- server/controllers/joinRoom.ts
+++ server/controllers/joinRoom.ts
@@ -1,15 +1,33 @@
 import { Request, Response } from "express";
 import mongoose from "mongoose";
+import roomModel from "../models/Task";
 
-export const joinRoom = (req: Request, res: Response) => {
-  const { id } = req.body;
+export const joinRoom = async (req: Request, res: Response) => {
+  const { id, clientUsername } = req.body;
 
   try {
     const isValidId = mongoose.Types.ObjectId.isValid(id);
 
-    if (isValidId) return res.sendStatus(200);
+    if (!isValidId) throw new Error("Invalid Id");
 
-    throw new Error("Invalid Id");
+    const room = await roomModel.findById(id);
+
+    if (!room) throw new Error("Room not exists");
+
+    const { roomUsers } = room;
+
+    for (const roomUser of roomUsers) {
+      const { username } = roomUser;
+
+      if (username === clientUsername) {
+        return res.sendStatus(200);
+      }
+    }
+
+    room.roomUsers.push({ username: clientUsername, isAdmin: false });
+    await room.save();
+
+    return res.sendStatus(200);
   } catch (error) {
     console.error(error);
     return res.sendStatus(406);
