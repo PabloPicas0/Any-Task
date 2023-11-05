b35e4b074c27172997e2e8136ff0b2527be1a769
PabloPicas0 <pawel.fe@gmail.com>
2023-11-05 16:51:10 +0100

    feat: add method for adding new tasks

--- client/src/UI/AddButton.tsx
+++ client/src/UI/AddButton.tsx
@@ -11,9 +11,40 @@ import {
 } from "@mui/material";
 
 import { useState } from "react";
+import { Form } from "react-router-dom";
+
+import { url } from "../Utils/api";
+
+import Error from "./Error";
 
 const AddButton = () => {
   const [open, setOpen] = useState<boolean>(false);
+  const [error, setError] = useState<boolean>(false);
+  const [taskDescription, setTaskDescription] = useState<string>("");
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      const req = await fetch(`${url}/api/create/task`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: `taskDescription=${taskDescription}&isActive=true&isBin=false`,
+      });
+
+      if (!req.ok) {
+        setError(true);
+      } else {
+        setOpen(false);
+      }
+
+      console.log(req);
+    } catch (error) {
+      console.error(error);
+      setError(true);
+    }
+  };
 
   return (
     <>
@@ -27,23 +58,35 @@ const AddButton = () => {
       </Tooltip>
 
       <Dialog onClose={() => setOpen(false)} open={open}>
-        <DialogTitle textAlign={"center"}>Add task</DialogTitle>
+        <Error isError={error} setError={setError} message="Failed to add task. Please try agian." />
 
-        <DialogContent>
-          <TextField label="Task description" margin="dense" />
-        </DialogContent>
+        <Form onSubmit={handleSubmit}>
+          <DialogTitle textAlign={"center"}>Add task</DialogTitle>
 
-        <DialogActions sx={{ justifyContent: "center" }}>
-          <Tooltip title="Add to task list">
-            <IconButton
-              sx={(theme) => ({
-                backgroundColor: theme.palette.grey[50],
-                boxShadow: theme.shadows[2],
-              })}>
-              <PostAdd fontSize="large" />
-            </IconButton>
-          </Tooltip>
-        </DialogActions>
+          <DialogContent>
+            <TextField
+              label="Task description"
+              margin="dense"
+              required
+              autoFocus
+              value={taskDescription}
+              onChange={(e) => setTaskDescription(e.target.value)}
+            />
+          </DialogContent>
+
+          <DialogActions sx={{ justifyContent: "center" }}>
+            <Tooltip title="Add to task list">
+              <IconButton
+                sx={(theme) => ({
+                  backgroundColor: theme.palette.grey[50],
+                  boxShadow: theme.shadows[2],
+                })}
+                type="submit">
+                <PostAdd fontSize="large" />
+              </IconButton>
+            </Tooltip>
+          </DialogActions>
+        </Form>
       </Dialog>
     </>
   );
