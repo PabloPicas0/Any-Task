4a743d9741666650fe96c542cd1e47d8ae0fec4c
PabloPicas0 <pawel.fe@gmail.com>
2023-11-09 19:15:02 +0100

    feat: add types to todo object, fix bug with push to never[], done deleteTask controller

--- server/controllers/createRoom.ts
+++ server/controllers/createRoom.ts
@@ -1,6 +1,15 @@
 import { Request, Response } from "express";
+import mongoose from "mongoose";
 
 import roomModel from "../models/Task";
+
+type TODO = {
+  description: string;
+  isActive: boolean;
+  isBin: boolean;
+  _id: mongoose.Types.ObjectId;
+  comments: [];
+};
 
 export const createRoom = async (req: Request, res: Response) => {
   const { username, allowNewTasks, editTasks, editPermissions } = req.body;
@@ -37,16 +46,19 @@ export const createTask = async (req: Request, res: Response) => {
   try {
     const room = await roomModel.findById(roomId);
 
-    const todo = {
+    if (!room) return res.sendStatus(404);
+
+    const todo: TODO = {
       description: taskDescription,
-      isActive: isActive,
-      isBin: isBin,
+      isActive: JSON.parse(isActive),
+      isBin: JSON.parse(isBin),
+      _id: new mongoose.Types.ObjectId(),
       comments: [],
     };
 
-    room?.tasks.active.push(todo);
+    room.tasks.active.push(todo);
 
-    await room?.save();
+    await room.save();
 
     return res.sendStatus(200);
   } catch (error) {
@@ -56,8 +68,30 @@ export const createTask = async (req: Request, res: Response) => {
 };
 
 export const deleteTask = async (req: Request, res: Response) => {
+  const { taskId } = req.body;
+
   try {
-    console.log(req.body);
+    const room = await roomModel.findOne({
+      "tasks.active": { $elemMatch: { _id: taskId } },
+    });
+
+    if (!room) return res.sendStatus(404);
+
+    const { active, bin } = room.tasks;
+
+    for (let i = 0; i < active.length; ++i) {
+      const { _id } = active[i];
+
+      if (_id.toString() === taskId) {
+        console.log(`Task Found ${_id.toString()}`);
+        const deletedTodo = room.tasks.active.splice(i, 1);
+        room.tasks.bin = [...bin, ...deletedTodo];
+      }
+    }
+    
+    await room.save();
+
+    return res.sendStatus(200);
   } catch (error) {
     console.log(error);
   }
