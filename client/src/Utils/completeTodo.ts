6666cafcd02c829bb5bdcf32d71bb8f59c24f114
PabloPicas0 <pawel.fe@gmail.com>
2023-11-11 19:40:10 +0100

    feat: create completeTodo function for sending completed tasks

---
+++ client/src/Utils/completeTodo.ts
@@ -0,0 +1,22 @@
+import { url } from "./api";
+
+const completeTodo = async (todoId: FormDataEntryValue | null, isActive: FormDataEntryValue | null) => {
+  try {
+    console.log(isActive);
+    const req = await fetch(`${url}/api/create/task`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: `todoId=${todoId}&isActive=${isActive}`,
+    });
+
+    console.log(req);
+
+    if (!req.ok) throw new Error("Something went wrong");
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export default completeTodo;
