3d1fdb1016078e949c50dddf522f47b1603130d3
PabloPicas0 <pawel.fe@gmail.com>
2023-11-14 22:06:46 +0100

    feat: room route action can handle multiple actions

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -12,6 +12,9 @@ import ErrorPage from "./Routes/ErrorPage.tsx";
 
 import { getRoomDetails } from "./Utils/getRoomDetails.ts";
 import updateTodo from "./Utils/updateTodo.ts";
+import deleteTodo from "./Utils/deleteTodo.ts";
+import completeTodo from "./Utils/completeTodo.ts";
+import addCommnet from "./Utils/addCommnet.ts";
 
 const router = createBrowserRouter([
   {
@@ -44,12 +47,46 @@ const router = createBrowserRouter([
       return { roomDetails };
     },
     action: async ({ request, params }) => {
-      const data = await request.formData();
-      const todo = data.get("todo");
+      const formData = await request.formData();
+      const intent = formData.get("intent");
 
-      if (!params.id || !todo) throw new Error("Invalid request");
+      if (intent === "add") {
+        const todo = formData.get("todo");
 
-      return await updateTodo(params.id, todo);
+        if (!params.id || !todo) throw new Error("Invalid request");
+
+        await updateTodo(params.id, todo);
+
+        return { ok: true };
+      }
+
+      if (intent === "delete") {
+        const taskId = formData.get("taskId");
+
+        await deleteTodo(taskId);
+
+        return { ok: true };
+      }
+
+      if (intent === "complete") {
+        const taskId = formData.get("taskId");
+        const isActive = formData.get("isActive");
+
+        await completeTodo(taskId, isActive);
+
+        return { ok: true };
+      }
+
+      if (intent === "comment") {
+        const url = new URL(request.url);
+        const username = url.searchParams.get("username");
+
+        await addCommnet(formData, username);
+
+        return { ok: true };
+      }
+
+      return { ok: false };
     },
   },
 ]);
